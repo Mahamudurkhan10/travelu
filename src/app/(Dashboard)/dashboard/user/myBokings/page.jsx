4dd13eb8aef3367fb0309634a7daa5b019a87dfb
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-10-10 03:38:22 +0600

    user dashboard working

--- src/app/(Dashboard)/dashboard/user/myBokings/page.jsx
+++ src/app/(Dashboard)/dashboard/user/myBokings/page.jsx
@@ -11,8 +11,8 @@ const MyBoking = () => {
    
           const handleDelete = async (id) => {
                try {
-                    Swal.fire({
-                         title: "Are you sure?",
+                    Swal.fire({ 
+                        title: "Are you sure?",
                          text: "You won't be able to revert this!",
                          icon: "warning",
                          showCancelButton: true,
